308637e050f6beb00aef15e16551b55e440778dd
dhvanan <dhvanan@gmail.com>
2017-03-03 08:28:19 -0800

    init updated

--- etk/data_extractors/__init__.py
+++ etk/data_extractors/__init__.py
@@ -2,10 +2,3 @@ from dictionary_extractor import populate_trie, extract_using_dictionary
 from address_extractor import extract_address
 from age_extractor import extract_age
 
-def init():
-	"""
-	Initialization method.
-	Returns:
-		object: RLTK object
-	"""
-	return Core()
