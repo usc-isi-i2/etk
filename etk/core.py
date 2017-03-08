4e1948c3ddf298208c4b3309b46e6d0e0e1c3af1
Muthu <raju.spm@gmail.com>
2017-03-08 13:15:31 -0800

    syntax fixes

--- etk/core.py
+++ etk/core.py
@@ -1,8 +1,7 @@
 # import all extractors
 from data_extractors import *
-from structured_extractors import *
+from structured_extractors import ReadabilityExtractor
 import json
-import codecs
 import gzip
 import os
 
@@ -13,11 +12,11 @@ class Core(object):
 	path_to_dig_dict = os.path.dirname(os.path.abspath(__file__)) + "/dictionaries/"
 
 	paths = {
-	"cities": path_to_dig_dict + "curated_cities.json.gz",
-	"haircolor": path_to_dig_dict + "haircolors-customized.json.gz",
-    "eyecolor": path_to_dig_dict + "eyecolors-customized.json.gz",
-    "ethnicities": path_to_dig_dict + "ethnicities.json.gz",
-    "names": path_to_dig_dict + "female-names-master.json.gz"
+		"cities": path_to_dig_dict + "curated_cities.json.gz",
+		"haircolor": path_to_dig_dict + "haircolors-customized.json.gz",
+		"eyecolor": path_to_dig_dict + "eyecolors-customized.json.gz",
+		"ethnicities": path_to_dig_dict + "ethnicities.json.gz",
+		"names": path_to_dig_dict + "female-names-master.json.gz"
 	}
 
 	tries = dict()
@@ -28,33 +27,31 @@ class Core(object):
 		trie = populate_trie(map(lambda x: x.lower(), values))
 		return trie
 
-	def load_dictionaries(self, paths = paths):
+	def load_dictionaries(self, paths=paths):
 		for key, value in paths.iteritems():
 			self.tries[key] = self.load_trie(value)
 
-	def extract_using_dictionary(self, tokens, name, 
-									pre_process = lambda x: x.lower(), 
-									pre_filter = lambda x: x, 
-									post_filter = lambda x: isinstance(x, basestring),
-									ngrams = 1,
-									joiner = ' '):
+	def extract_using_dictionary(self, tokens, name, pre_process=lambda x: x.lower(),
+									pre_filter=lambda x: x,
+									post_filter=lambda x: isinstance(x, basestring),
+									ngrams=1,
+									joiner=' '):
 		""" Takes in tokens as input along with the dict name"""
 
 		if name in self.tries:
-			return extract_using_dictionary(tokens, pre_process = pre_process, 
-												trie=self.tries[name], 
-												pre_filter = pre_filter,
-												post_filter = post_filter, 
-												ngrams = ngrams, 
-												joiner = joiner)
+			return extract_using_dictionary(tokens, pre_process=pre_process,
+												trie=self.tries[name],
+												pre_filter=pre_filter,
+												post_filter=post_filter,
+												ngrams=ngrams,
+												joiner=joiner)
 		else:
 			print "wrong dict"
 			return []
 
 	def extract_address(self, document):
-		""" 
-		Takes text document as input. 
-		
+		"""
+		Takes text document as input.
 		Note:
 		1. Add keyword list as a user parameter
 		2. Add documentation
@@ -70,13 +67,11 @@ class Core(object):
 	def extract_table(self, html_doc):
 		return table_extract(html_doc)
 
-
-	def extract_age(self,doc):
+	def extract_age(self, doc):
 		return age_extract(doc)
 
-	def extract_weight(self,doc):
-		return weight_extract(doc)	
+	def extract_weight(self, doc):
+		return weight_extract(doc)
 
-	def extract_height(self,doc):
-		return height_extract(doc)	
-
+	def extract_height(self, doc):
+		return height_extract(doc)
