eaab7c4076e4e9494c4ed5b414ab2d7a14c3d948
Muthu <raju.spm@gmail.com>
2017-03-10 10:46:11 -0800

    running with configuration file for paths

--- examples/example_readability.py
+++ examples/example_readability.py
@@ -42,25 +42,69 @@ def buildContent(jl, doc):
     extractors['tables'] = tk.extract_table(jl['raw_content'])
 
 
-def buildTokensAndData(jl, extractors):
+def buildTokensAndData(jl, extractors, config):
     # for readability and title
-    jsonpath_expr = parse('*.text.`parent`')
-    matches = jsonpath_expr.find(extractors)
-    for match in matches:
-        processDataMatch(match)
+    if not config['extractions']:
+        raise "No extractions mentioned in config file"
 
-    # for tables
-    if 'tables' in extractors.keys() and extractors['tables']:
-        jsonpath_expr = parse('tables[*].rows[*].cells[*].text.`parent`')
-        matches = jsonpath_expr.find(extractors)
-        for match in matches:
-            # pass
-            processDataMatch(match)
+    for extractions in config['extractions']:
+        for path in extractions['input']:
+            print path
+            jsonpath_expr = parse(path)
+            try:
+                matches = jsonpath_expr.find(extractors)
+            except Exception:
+                print "No matches for ", path
+                matches = []
+            for match in matches:
+                processDataMatch(match, extractions)
+
+    # # for tables
+    # if 'tables' in extractors.keys() and extractors['tables']:
+    #     jsonpath_expr = parse('tables[*].rows[*].cells[*].text.`parent`')
+    #     matches = jsonpath_expr.find(extractors)
+    #     for match in matches:
+    #         # pass
+    #         processDataMatch(match)
 
 
-def processDataMatch(match):
-    match.value['crf_tokens'] = tk.extract_crftokens(match.value['text'])
-    match.value['tokens'] = tk.extract_tokens_from_crf(match.value['crf_tokens'])
+def annotateTokenToExtractions(tokens, extractions):
+    for extractor, extractions in extractions.iteritems():
+        if extractor in ['phone']:
+            " ignoring phone annotation.."
+            continue
+        for extraction in extractions:
+            input_type = extraction['input_type']
+            if 'text' in input_type:
+                # build text tokens
+                pass
+
+            if 'tokens' not in input_type:
+                print "ignoring ", extractor, " as tokens not dependant.."
+                continue
+            data = extraction['result']
+            for values in data:
+                start = values['context']['start']
+                end = values['context']['end']
+                offset = 0
+                for i in range(start, end):
+                    if 'semantic_type' not in tokens[i].keys():
+                        tokens[i]['semantic_type'] = []
+                    temp = {}
+                    temp['type'] = extractor
+                    temp['offset'] = offset
+                    if offset == 0:
+                        temp['length'] = end - start
+                    tokens[i]['semantic_type'].append(temp)
+                    offset += 1
+    return tokens
+
+
+def processDataMatch(match, extractions):
+    if 'crf_tokens' not in match.value.keys():
+        match.value['crf_tokens'] = tk.extract_crftokens(match.value['text'])
+    if 'tokens' not in match.value.keys():
+        match.value['tokens'] = tk.extract_tokens_from_crf(match.value['crf_tokens'])
 
     data_extractors = {}
     data_extractors['city'] = tk.extract_using_dictionary(match.value['crf_tokens'], name='cities', ngrams=2)
@@ -70,21 +114,25 @@ def processDataMatch(match):
     data_extractors['name'] = tk.extract_using_dictionary(match.value['tokens'], name='names')
     data_extractors['address'] = tk.extract_address(match.value['text'])
 
+    # for extractor in data_extractors:
+    #     if data_extractors[extractor]:
+    #         annotateTokenToExtractions(match.value['crf_tokens'], data_extractors[extractor])
+
     match.value['data_extractors'] = data_extractors
 
 if __name__ == "__main__":
-
     input_path = sys.argv[1]
     output_file = sys.argv[2]
+    config_file = sys.argv[3]
+    config = load_json_file(config_file)
 
     o = codecs.open(output_file, 'w', 'utf-8')
     for jl in jl_file_iterator(input_path):
         extractors = {}
         # Content extractors
         buildContent(jl, extractors)
-        # print extractors
         # tokens and data
-        buildTokensAndData(jl, extractors)
+        buildTokensAndData(jl, extractors, config)
 
         jl['extractors'] = extractors
         jl['raw_content'] = '...'
