0c7280cba59583e76ccf0e596defa54001f56a9b
Amandeep Singh <amandeep.s.saggu@gmail.com>
2017-03-30 14:18:36 -0700

    compliant price extractor output in development

---
+++ etk/data_extractors/digPriceExtractor/digpe/unit/price_units.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# @Author: ZwEin
+# @Date:   2016-07-04 11:52:32
+# @Last Modified by:   ZwEin
+# @Last Modified time: 2016-07-04 11:52:50
+
+
+UNIT_PRICE_UNITS = [
+    '\$',
+    'dollar',
+    'euro',
+    'nuck',
+    'rose',
+    'candy',
+    'donation'
+]
