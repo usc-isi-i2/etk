5f4604fb2ac6765594d49c0154ff1178b8ca2925
Muthu <raju.spm@gmail.com>
2017-04-02 19:18:06 -0700

    added multiprocessing code with cleaning and restructure

---
+++ examples/etk_pipeline_multiprocessing.py
@@ -0,0 +1,194 @@
+"""
+Class for running etk pipeline in parallel using a config file
+"""
+
+import sys, os
+import json
+import codecs
+import fnmatch
+from multiprocessing import Process
+import multiprocessing as mp
+from jsonpath_rw import parse
+import time
+# TODO: Remove in future once etk published
+sys.path.insert(1, os.path.join(sys.path[0], '..'))
+import etk
+
+
+class ETKParallelProcess:
+    def __init__(self, configuration_file, input_file, output_file):
+        self.config = self.load_json_file(configuration_file)
+        self.input_file = input_file
+        self.output_file = self.open_file(output_file)
+        self.record_count = 0
+        # library
+        self.tk = ''
+        self.__intialize_libraries()
+
+    def open_file(self, file_name):
+        return codecs.open(file_name, 'w', 'utf-8')
+
+    def load_json_file(self, file_name):
+        rules = json.load(codecs.open(file_name, 'r', 'utf-8'))
+        return rules
+
+    def __intialize_libraries(self):
+        self.tk = etk.init()
+        self.tk.load_dictionaries()
+
+    def process_wrapper(self, chunkStart, chunkSize):
+        with open(self.input_file) as f:
+            f.seek(chunkStart)
+            lines = f.read(chunkSize).splitlines()
+            for line in lines:
+                document = json.loads(line)
+                self.process_file(document)
+
+    # Splitting data into chunks for parallel processing
+    def chunkify(self, filename, size=1024*1024):
+        fileEnd = os.path.getsize(filename)
+        with open(filename,'r') as f:
+            chunkEnd = f.tell()
+            while True:
+                chunkStart = chunkEnd
+                f.seek(size,1)
+                f.readline()
+                chunkEnd = f.tell()
+                yield chunkStart, chunkEnd - chunkStart
+                if chunkEnd > fileEnd:
+                    break
+
+    def process_file(self, jl):
+        extractors = {}
+        # Content extractors
+        self.build_content(jl, extractors)
+        # tokens and data
+        self.build_tokens_and_data(jl, extractors)
+
+        jl['extractors'] = extractors
+        jl['raw_content'] = '...'
+        self.output_file.write(json.dumps(jl) + '\n')
+
+    def build_content(self, jl, extractors):
+        extractors['content_relaxed'] = self.tk.extract_readability(jl['raw_content'], {'recall_priority': True})
+        extractors['content_strict'] = self.tk.extract_readability(jl['raw_content'], {'recall_priority': False})
+        extractors['tables'] = self.tk.extract_table(jl['raw_content'])
+
+
+    def build_tokens_and_data(self, jl, extractors):
+        # for readability and title
+        if not self.config['extractions']:
+            raise "No extractions mentioned in config file"
+
+        for extractions in self.config['extractions']:
+            for path in extractions['input']:
+                jsonpath_expr = parse(path)
+                try:
+                    matches = jsonpath_expr.find(extractors)
+                except Exception:
+                    print "No matches for ", path
+                    matches = []
+                for match in matches:
+                    self.process_data_match(match, extractions)
+
+    @staticmethod
+    def annotate_token_to_extractions(tokens, extractions):
+        for extractor, extraction in extractions.iteritems():
+            input_type = extraction['input_type']
+            if 'text' == input_type:
+                # build text tokens
+                continue
+
+            data = extraction['result']
+            for values in data:
+                start = values['context']['start']
+                end = values['context']['end']
+                offset = 0
+                for i in range(start, end):
+                    if 'semantic_type' not in tokens[i].keys():
+                        tokens[i]['semantic_type'] = []
+                    temp = {'type': extractor, 'offset': offset}
+                    if offset == 0:
+                        temp['length'] = end - start
+                    tokens[i]['semantic_type'].append(temp)
+                    offset += 1
+
+    def process_data_match(self, match, extractions):
+        if 'tokens' not in match.value.keys():
+            match.value['tokens'] = self.tk.extract_crftokens(match.value['text'])
+        if 'simple_tokens' not in match.value.keys():
+            match.value['simple_tokens'] = self.tk.extract_tokens_from_crf(match.value['tokens'])
+
+        data_extractors = {}
+        for extractor in extractions['extractors']:
+            extractor_info = extractions['extractors'][extractor]
+
+            try:
+                function_call = getattr(self.tk, extractor_info['extractor'])
+            except Exception:
+                print extractor_info['extractor'], " fucntion not found in etk"
+                continue
+            try:
+                # initialize the data extractor
+                data_extractors[extractor] = {'input_type': 'tokens', 'extractor': extractor}
+
+                if extractor_info['input_type'] == 'tokens' and 'name' in extractor_info['config'].keys() and 'ngrams' in extractor_info['config'].keys():
+                    data_extractors[extractor]['result'] = function_call(match.value['tokens'], name=extractor_info['config']['name'], ngrams=extractor_info['config']['ngrams'])
+                elif extractor_info['input_type'] == 'tokens' and 'name' in extractor_info['config'].keys():
+                    data_extractors[extractor]['result'] = function_call(match.value['simple_tokens'], name=extractor_info['config']['name'])
+                elif extractor_info['input_type'] == 'text':
+                    data_extractors[extractor]['result'] = function_call(match.value['text'])
+                else:
+                    print "No matching call found for input type - ", extractor_info['input_type']
+            except Exception as e:
+                data_extractors[extractor]['result'] = ''
+                print extractor, " crashed - ", e
+
+        #  CLEAN THE EMPTY DATA EXTRACTORS
+        data_extractors = dict((key, value) for key, value in data_extractors.iteritems() if value['result'])
+        if data_extractors:
+            self.annotate_token_to_extractions(match.value['tokens'], data_extractors)
+            match.value['data_extractors'] = data_extractors
+
+    def run_parallel(self, processes=4):
+        processes = int(processes)
+        pool = mp.Pool(processes)
+        try:
+            pool = mp.Pool(processes)
+            jobs = []
+            # run for chunks of files
+            for chunkStart,chunkSize in self.chunkify(input_path):
+                jobs.append(pool.apply_async(self.process_wrapper,(chunkStart,chunkSize)))
+            for job in jobs:
+                job.get()
+            pool.close()
+        except Exception as e:
+            print e
+
+    def jl_file_iterator(self, file):
+        with codecs.open(file, 'r', 'utf-8') as f:
+            for line in f:
+                document = json.loads(line)
+                yield document
+
+    def run_serial(self):
+        for jl in self.jl_file_iterator(input_path):
+            self.process_file(jl)
+
+
+if __name__ == "__main__":
+    # read arguments
+    input_path = sys.argv[1]
+    output_file = sys.argv[2]
+    config_file = sys.argv[3]
+
+    ep = ETKParallelProcess(config_file, input_path, output_file)
+    startTime = time.time()
+
+    if len(sys.argv) == 5:
+        ep.run_parallel(sys.argv[4])
+    else:
+        ep.run_serial()
+    print('The script took {0} second !'.format(time.time() - startTime))
+
+
--- examples/readability_multiprocessing.py
+++ examples/readability_multiprocessing.py
@@ -1,10 +1,11 @@
-# This is how we intend to use it 
+# This is how we intend to use it
 import sys
 import os
 import json
 import codecs
 import fnmatch
 from multiprocessing import Process
+import multiprocessing as mp,os
 from jsonpath_rw import parse
 import time
 sys.path.insert(1, os.path.join(sys.path[0], '..'))
@@ -13,6 +14,7 @@ import etk
 
 tk = etk.init()
 tk.load_dictionaries()
+
 
 
 def load_json_file(file_name):
@@ -37,13 +39,13 @@ def jl_path_iterator(file_path):
         yield abs_file_path
 
 
-def buildContent(jl, extractors):
+def build_content(jl, extractors):
     extractors['content_relaxed'] = tk.extract_readability(jl['raw_content'], {'recall_priority': True})
     extractors['content_strict'] = tk.extract_readability(jl['raw_content'], {'recall_priority': False})
     extractors['tables'] = tk.extract_table(jl['raw_content'])
 
 
-def buildTokensAndData(jl, extractors):
+def build_tokens_and_data(jl, extractors):
     # for readability and title
     if not config['extractions']:
         raise "No extractions mentioned in config file"
@@ -57,10 +59,10 @@ def buildTokensAndData(jl, extractors):
                 print "No matches for ", path
                 matches = []
             for match in matches:
-                processDataMatch(match, extractions)
+                process_data_match(match, extractions)
 
 
-def annotateTokenToExtractions(tokens, extractions):
+def annotate_token_to_extractions(tokens, extractions):
     for extractor, extraction in extractions.iteritems():
         input_type = extraction['input_type']
         if 'text' == input_type:
@@ -75,16 +77,14 @@ def annotateTokenToExtractions(tokens, extractions):
             for i in range(start, end):
                 if 'semantic_type' not in tokens[i].keys():
                     tokens[i]['semantic_type'] = []
-                temp = {}
-                temp['type'] = extractor
-                temp['offset'] = offset
+                temp = {'type': extractor, 'offset': offset}
                 if offset == 0:
                     temp['length'] = end - start
                 tokens[i]['semantic_type'].append(temp)
                 offset += 1
 
 
-def processDataMatch(match, extractions):
+def process_data_match(match, extractions):
     if 'tokens' not in match.value.keys():
         match.value['tokens'] = tk.extract_crftokens(match.value['text'])
     if 'simple_tokens' not in match.value.keys():
@@ -100,7 +100,7 @@ def processDataMatch(match, extractions):
             print extractor_info['extractor'], " fucntion not found in etk"
             continue
         try:
-            # intitalize the data extractor
+            # initialize the data extractor
             data_extractors[extractor] = {'input_type': 'tokens', 'extractor': extractor}
 
             if extractor_info['input_type'] == 'tokens' and 'name' in extractor_info['config'].keys() and 'ngrams' in extractor_info['config'].keys():
@@ -118,20 +118,21 @@ def processDataMatch(match, extractions):
     #  CLEAN THE EMPTY DATA EXTRACTORS
     data_extractors = dict((key, value) for key, value in data_extractors.iteritems() if value['result'])
     if data_extractors:
-        annotateTokenToExtractions(match.value['tokens'], data_extractors)
+        annotate_token_to_extractions(match.value['tokens'], data_extractors)
         match.value['data_extractors'] = data_extractors
 
 
-def processFile(jl):
+def process_file(jl):
     extractors = {}
     # Content extractors
-    buildContent(jl, extractors)
+    build_content(jl, extractors)
     # tokens and data
-    buildTokensAndData(jl, extractors)
+    build_tokens_and_data(jl, extractors)
 
     jl['extractors'] = extractors
     jl['raw_content'] = '...'
     o.write(json.dumps(jl) + '\n')
+
 
 
 def write_output(out, results):
@@ -143,45 +144,99 @@ def write_output_record(out, result):
     o.write(json.dumps(result) + '\n')
 
 
-def parallelProcess(files, outfile):
+def parallel_process(files, outfile):
     processes = []
     for i in range(len(files)):
-        p = Process(target=processFile, args=(files[i],))
+        p = Process(target=process_file, args=(files[i],))
         processes.append(p)
         p.start()
     for i in range(len(files)):
         processes[i].join()
 
+def process_wrapper(chunkStart, chunkSize):
+    with open(input_path) as f:
+        f.seek(chunkStart)
+        lines = f.read(chunkSize).splitlines()
+        for line in lines:
+            document = json.loads(line)
+            process_file(document)
+
+# Splitting data into chunks for parallel processing
+def chunkify(fname, size=1024*1024):
+    fileEnd = os.path.getsize(fname)
+    with open(fname,'r') as f:
+        chunkEnd = f.tell()
+        while True:
+            chunkStart = chunkEnd
+            f.seek(size,1)
+            f.readline()
+            chunkEnd = f.tell()
+            yield chunkStart, chunkEnd - chunkStart
+            if chunkEnd > fileEnd:
+                break
+
 if __name__ == "__main__":
+    # read arguments
     input_path = sys.argv[1]
     output_file = sys.argv[2]
     config_file = sys.argv[3]
+
     config = load_json_file(config_file)
     o = codecs.open(output_file, 'w', 'utf-8')
     startTime = time.time()
 
     if len(sys.argv) == 5:
-        threads = int(sys.argv[4])
-        print threads
-        files, i = [], 1
-        # pool = Pool(processes=threads)
-        for jl in jl_file_iterator(input_path):
-            files.append(jl)
-            if i % (threads * threads) == 0:
-                # pool.map(processFile, files)
-                parallelProcess(files, o)
-                files = []
-            i += 1
+        ### Code for running using apply_async
+        try:
+            processes = int(sys.argv[4])
+            pool = mp.Pool(processes)
+            jobs = []
+            # run for chunks of files
+            for chunkStart,chunkSize in chunkify(input_path):
+                jobs.append(pool.apply_async(process_wrapper,(chunkStart,chunkSize)))
 
-        if files:
-            # pool.map(processFile, files)
-            parallelProcess(files, o)
-            files = []
+            for job in jobs:
+                job.get()
+
+            pool.close()
+        except Exception as e:
+            print e
     else:
         for jl in jl_file_iterator(input_path):
-            results = processFile(jl)
+            results = process_file(jl)
             write_output_record(o, results)
     print('The script took {0} second !'.format(time.time() - startTime))
+
+
+
+
+
+#init objects
+
+
+#Code for running using process threads
+
+# try:
+#     threads = int(sys.argv[4])
+#     pool = mp.Pool(threads)
+#     jobs = []
+#
+#     i = 1
+#     for chunkStart,chunkSize in chunkify(input_path):
+#         p = mp.Process(target=process_wrapper, args=(chunkStart,chunkSize))
+#         p.start()
+#         jobs.append(p)
+#
+#     #wait for all jobs to finish
+#     for job in jobs:
+#         job.join()
+#
+#     pool.close()
+# except Exception as e:
+#     print e
+
+
+
 
     # for jl in jl_file_iterator(input_path):
     #     files.append(jl)
