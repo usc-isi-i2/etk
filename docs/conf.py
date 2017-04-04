fe892d198645d74d13ff8bf414fd59e5e37e3dc9
Amandeep Singh <amandeep.s.saggu@gmail.com>
2017-04-03 17:56:33 -0700

    more docs for api reference

--- docs/conf.py
+++ docs/conf.py
@@ -16,9 +16,12 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../etk'))
+sys.path.insert(0, os.path.abspath('/Users/amandeep/anaconda2/envs/memexeval2017/lib/python2.7/site-packages'))
+
+# from etk.core import Core
 
 # -- General configuration ------------------------------------------------
 
