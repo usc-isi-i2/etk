4c65b250dc0e9788d16342903fa3aef693a630f4
Amandeep Singh <amandeep.s.saggu@gmail.com>
2017-05-19 15:52:17 -0700

    handle inferlink posts

--- etk/data_extractors/landmark_extraction.py
+++ etk/data_extractors/landmark_extraction.py
@@ -18,13 +18,20 @@ def extract(html, url, extractionrulesall, threshold=0.5):
         if rules is not None:
             extraction_list = rules.extract(html)
             flatten = flattenResult(extraction_list)
-            for key in flatten.keys():
-                if flatten[key].strip() != '':
-                    inferlink_extraction[key] = flatten[key]
+            if flatten:
+                if "posts" in flatten:
+                    # this is a forum kind of page, handle differently
+                    if len(flatten['posts']) > 0:
+                        return flatten['posts']
+                else:
+                    for key in flatten.keys():
+                        if flatten[key].strip() != '':
+                            inferlink_extraction[key] = flatten[key]
         properly_extracted_fields = len(inferlink_extraction)
         if properly_extracted_fields > 0 and float(properly_extracted_fields) / float(number_of_rules) >= threshold:
             # print 'rules  %s succeeded for  %s' % (matched_rule_key, url)
             # print '%s rules matched out of %s' % (properly_extracted_fields, number_of_rules)
+            # print inferlink_extraction
             return inferlink_extraction
         else:
             # if matched_rule_key:
