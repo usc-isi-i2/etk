aa0ce1185ea25f328ef6c1be9dbad4ef33a3ca2f
Amandeep Singh <amandeep.s.saggu@gmail.com>
2017-07-06 17:39:00 -0700

    if etk fails, return NOne

--- run_etk_spark.py
+++ run_etk_spark.py
@@ -38,8 +38,10 @@ if __name__ == '__main__':
     conf = SparkConf()
     extraction_config = json.load(codecs.open(extraction_config_path))
     c = Core(extraction_config=extraction_config)
-    input_rdd = sc.sequenceFile(input_path).partitionBy(1000)
+    input_rdd = sc.sequenceFile(input_path)#.partitionBy(1000)
+    print input_rdd.first()
     input_rdd = input_rdd.mapValues(json.loads).filter(lambda x: remove_if_no_html(x[1])).mapValues(add_doc_id)\
-        .mapValues(lambda x: c.process(x, create_knowledge_graph=True)).mapValues(json.dumps)
+        .mapValues(lambda x: c.process(x, create_knowledge_graph=True))
+    input_rdd = input_rdd.filter(lambda x: x[1] is not None).mapValues(json.dumps)
     input_rdd.saveAsSequenceFile(output_path, compressionCodecClass=compression)
 
