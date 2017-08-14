1df0cf417a074a8789435010b0196ce34ae03bb4
Amandeep Singh <amandeep.s.saggu@gmail.com>
2017-08-14 13:50:38 -0700

    return NOne if date is in future

--- etk/data_extractors/date_parser.py
+++ etk/data_extractors/date_parser.py
@@ -1,9 +1,13 @@
 import dateparser
+import datetime
 
 
-def parse_date(str_date):
+def parse_date(str_date, ignore_future_dates=True):
     try:
-        return dateparser.parse(str_date)
+        parsed_date = dateparser.parse(str_date)
+        if ignore_future_dates:
+            return parsed_date if datetime.datetime.now() >= parsed_date else None
+        return parsed_date
     except Exception as e:
         print 'Exception: {}, failed to parse {} as date'.format(e, str_date)
         return None
@@ -15,4 +19,3 @@ def convert_to_iso_format(date):
     except Exception as e:
         print 'Exception: {}, failed to convert {} to isoformat '.format(e, date)
         return None
-
