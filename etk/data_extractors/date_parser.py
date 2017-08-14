9e5eb70befa859372f353df08a4dcaf215ebc820
Amandeep Singh <amandeep.s.saggu@gmail.com>
2017-08-14 16:52:48 -0700

    do no parse impartial dates

--- etk/data_extractors/date_parser.py
+++ etk/data_extractors/date_parser.py
@@ -2,11 +2,15 @@ import dateparser
 import datetime
 
 
-def parse_date(str_date, ignore_future_dates=True):
+def parse_date(str_date, ignore_future_dates=True, strict_parsing=True):
     try:
-        parsed_date = dateparser.parse(str_date)
-        if ignore_future_dates:
-            return parsed_date if datetime.datetime.now() >= parsed_date else None
+        if strict_parsing:
+            parsed_date = dateparser.parse(str_date, settings={'STRICT_PARSING': True})
+        else:
+            parsed_date = dateparser.parse(str_date)
+        if parsed_date:
+            if ignore_future_dates:
+                return parsed_date if datetime.datetime.now() >= parsed_date else None
         return parsed_date
     except Exception as e:
         print 'Exception: {}, failed to parse {} as date'.format(e, str_date)
