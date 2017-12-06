11ae748cf5b9481d92e1db42165b84a4dab9d888
Amandeep Singh <amandeep.s.saggu@gmail.com>
2017-12-06 10:15:43 -0800

    add another unitest for post filter, jic

--- etk/unit_tests/test_content_extractions.py
+++ etk/unit_tests/test_content_extractions.py
@@ -433,5 +433,60 @@ class TestExtractions(unittest.TestCase):
         self.assertTrue(len(r['knowledge_graph']['actor_title']) == 1)
         self.assertTrue(r['knowledge_graph']['actor_title'][0]['value'] == 'noncombatant status not contested'.upper())
 
+    def test_extract_as_is_post_filter_2(self):
+        doc = {
+            "uri": "1",
+            "event_actors": [
+                {
+                    "description": "Non-State, Internal, No State Sanction",
+                    "id": "internalnononstatesanctionstate",
+                    "title": ""
+                },
+                {
+                    "description": "Noncombatant Status Asserted",
+                    "id": "assertedcontestednoncombatantnoncombatantnotstatusstatus",
+                    "title": "Noncombatant Status Not Contested"
+                }
+            ]
+        }
+
+        e_config = {
+            "extraction_policy": "replace",
+            "error_handling": "raise_error",
+            "document_id": "uri",
+            "content_extraction": {
+                "json_content": [
+                    {
+                        "input_path": "event_actors[*].title",
+                        "segment_name": "actor_title"
+                    }
+                ]
+            },
+            "data_extraction": [
+                {
+                    "input_path": "content_extraction.actor_title[*].text.`parent`",
+                    "fields": {
+                        "actor_title": {
+                            "extractors": {
+                                "extract_as_is": {
+                                    "extraction_policy": "keep_existing",
+                                    "config": {
+                                        "post_filter": [
+                                            "isinstance(x, dict)"
+                                        ]
+                                    }
+                                }
+                            }
+
+                        }
+                    }
+                }
+            ]
+        }
+        c = Core(extraction_config=e_config)
+        r = c.process(doc)
+        self.assertTrue('knowledge_graph' not in r)
+
+
 if __name__ == '__main__':
     unittest.main()
