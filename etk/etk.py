6ce92cfca1edeea3f19ce9d062f94b3c22f0f538
Pedro Szekely <szekely1401@gmail.com>
2018-02-23 19:56:57 -0800

    create_document now accepts object

--- etk/etk.py
+++ etk/etk.py
@@ -16,7 +16,7 @@ class ETK(object):
         Factory method to wrap input JSON docs in an ETK Document object.
 
         Args:
-            doc (dict): a JSON object containing a document in CDR format.
+            doc (object): a JSON object containing a document in CDR format.
             mime_type (str): if doc is a tring, the mime_type tells what it is
             url (str): if the doc came from the web, specifies the URL for it
 
