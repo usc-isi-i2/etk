277f11aa68deaba67803213eea0600319276f1f4
Pedro Szekely <szekely1401@gmail.com>
2018-02-23 19:56:40 -0800

    example HTML extraction

--- examples/html_basic/html_basic_extraction.py
+++ examples/html_basic/html_basic_extraction.py
@@ -1,27 +1,27 @@
-import json
 import os, sys
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.append(os.path.join(os.path.dirname(__file__), '../..'))
+
+import json
 from etk.etk import ETK
 from etk.extractors.html_content_extractor import HTMLContentExtractor
 from etk.extractors.html_metadata_extractor import HTMLMetadataExtractor
 
-sample_html = None # read sample file from disk
+
+sample_html = None  # read sample file from disk
 
 etk = ETK()
-doc = etk.create_document(sample_html, content_type = "HTML")
+doc = etk.create_document(sample_html, mime_type="text/html", url="http://ex.com/123")
 
 metadata_extractor = HTMLMetadataExtractor()
 content_extractor = HTMLContentExtractor()
 
-descriptions_path = etk.parser("projects[*].description")
-projects_path = etk.parser("projects[*]")
+root = doc.select_segments(etk.parser("$"))[0]
 
-descriptions = doc.select_segments(descriptions_path)
-projects = doc.select_segments(projects_path)
+# Passing arguments to extractors using keyword arguments in invoke_extractor is causing warnings.
+# Is there a pythonic way to do this?
+root.store_extractions(doc.invoke_extractor(metadata_extractor, extract_title=True), "title")
+root.store_extractions(doc.invoke_extractor(metadata_extractor, extract_meta=True), "metadata")
+root.store_extractions(doc.invoke_extractor(content_extractor, stategy=HTMLContentExtractor.Strategy.ALL_TEXT), "text")
 
-for d, p in zip(descriptions, projects):
-    names = doc.invoke_extractor(name_extractor, d)
-    p.store_extractions(names, "members")
-
-print(json.dumps(sample_input, indent=2))
+print(json.dumps(doc.cdr_document, indent=2))
