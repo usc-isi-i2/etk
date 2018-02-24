0382ac55c2d330cd566245ad947e43584cce2295
Pedro Szekely <szekely1401@gmail.com>
2018-02-23 19:52:46 -0800

    extend definition of create_document to support HTML

--- etk/etk.py
+++ etk/etk.py
@@ -11,19 +11,21 @@ class ETK(object):
         self.parser = jsonpath_rw.parse
         self.default_tokenizer = Tokenizer(nlp=spacy.load('en_core_web_sm'))
 
-    def create_document(self, doc: dict) -> Document:
+    def create_document(self, doc: object, mime_type: str=None, url: str="http://ex.com/123") -> Document:
         """
         Factory method to wrap input JSON docs in an ETK Document object.
 
         Args:
             doc (dict): a JSON object containing a document in CDR format.
+            mime_type (str): if doc is a tring, the mime_type tells what it is
+            url (str): if the doc came from the web, specifies the URL for it
 
         Returns: wrapped Document
 
         """
         return Document(self, doc)
 
-    def load_glossary(self, file_path) -> List[str]:
+    def load_glossary(self, file_path: str) -> List[str]:
         """
         A glossary is a text file, one entry per line.
 
