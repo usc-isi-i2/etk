0d0ea90665d4178565f1f62294bbbd1e42ef2386
Pedro Szekely <szekely1401@gmail.com>
2018-03-02 15:53:44 -0800

    cleanup imports

--- examples/hello_world/hello_world.py
+++ examples/hello_world/hello_world.py
@@ -1,5 +1,6 @@
 import json
-import os, sys
+import os
+import sys
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.append(os.path.join(os.path.dirname(__file__), '../..'))
 from etk.etk import ETK
