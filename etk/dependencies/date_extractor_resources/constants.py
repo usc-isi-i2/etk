b9450f8b56f1e5d5aab87bcdd772798d2ec1ff55
DongyuLi <dongyu@evestemptation.com>
2018-03-29 23:39:51 -0700

    finished the date_extractor, will add unittests

--- etk/dependencies/date_extractor_resources/constants.py
+++ etk/dependencies/date_extractor_resources/constants.py
@@ -1,22 +1,18 @@
-# reference: http://www.gnu.org/software/libc/manual/html_node/Formatting-Calendar-Time.html
-# symbol references: http://strftime.org/
-import re, json
 singleton_regex = {
     '%Y': r'([1-2][0-9][0-9][0-9])',  # year in four digits
     '%y': r'([6-9][0-9]|[0-3][0-9])',  # year in two digits
-    '%B': r'(January|February|March|April|May|June|July|August|September|October|November|December)',  # month
-    '%b': r'(Jan\.?|Feb\.?|Mar\.?|Apr\.?|Jun\.?|Jul\.?|Aug\.?|Sep(?:t?)\.?|Oct\.?|Nov\.?|Dec\.?)',  # month abbr.
-    '%m': r'(1[0-2]|0[1-9])',  # month in two digits: 01-12
-    '%-m': r'(1[0-2]|[1-9])',  # month in number without prefix 0: 1-12
-    '%d': r'(3[0-1]|[1-2][0-9]|0[1-9])',  # day in two digits: 01-31
-    '%-d': r'(3[0-1]|[1-2][0-9]|[1-9])',  # day in number without prefix 0: 1-31
+    '%B': r'(January|February|March|April|May|June|July|August|September|October|November|December|'
+          r'enero|febrero|marzo|abril|mayo|junio|julio|agosto|septiembre|octubre|noviembre|diciembre)',  # month
+    '%b': r'(Jan\.?|Feb\.?|Mar\.?|Apr\.?|Jun\.?|Jul\.?|Aug\.?|Sep(?:t?)\.?|Oct\.?|Nov\.?|Dec\.?|'
+          r'enero|feb\.?|marzo|abr\.?|mayo|jun\.?|jul\.?|agosto|sept\.?|set\.?|oct\.?|nov\.?|dic\.?)',  # month abbr.
+    '%m': r'(1[0-2]|0?[1-9])',  # month in two digits: 01-12 or just 0-12
+    '%d': r'(3[0-1]|[1-2][0-9]|0?[1-9])',  # day in two digits: 01-31 or juest 1-31
     'd_suffix': r'(?:st|nd|rd|th)',
-    '%A': r'(Monday|Tuesday|Wednesday|Thursday|Friday|Saturday|Sunday)',  # weekdays
-    '%a': r'(Mon\.?|Tue\.?|Wed\.?|Th(?:u(?:r(?:s?)?)?)\.?|Fri\.?|Sat\.?|Sun\.?)',  # weekdays abbr.
-    '%H': r'(2[0-3]|[0-1][0-9])',  # hour in 24-hours in two digits: 00-23
-    '%-H': r'(2[0-3]|1[0-9]|[0-9])',  # hour in 24-hour in number without prefix 0: 0-23
-    '%I': r'(1[0-2]|0[1-9])',  # hour in 12-hour in two digits: 01-12
-    '%-I': r'(1[0-2]|[1-9])',  # hour in 12-hour in number without prefix 0: 1-12
+    '%A': r'(Monday|Tuesday|Wednesday|Thursday|Friday|Saturday|Sunday|'
+          r'lunes|martes|miércoles|jueves|viernes|sábado|domingo)',  # weekdays
+    '%a': r'(Mon\.?|Tue\.?|Wed\.?|Thu(?:r(?:s?)?)\.?|Fri\.?|Sat\.?|Sun\.?|L|M|X|J|V|S|D)',  # weekdays abbr.
+    '%H': r'(2[0-3]|1[0-9]|0?[0-9])',  # hour in 24-hours in two digits: 00-23 or just 0-23
+    '%I': r'(1[0-2]|0?[1-9])',  # hour in 12-hour in two digits: 01-12 or just 1-12
     '%p': r'((?: ?)AM(?:\.?)|(?: ?)PM(?:\.?))',  # am/pm markers
     '%M': r'([0-5][0-9])',  # minute in two digits: 00-59
     '%S': r'([0-5][0-9])',  # second in two digits: 00-59
@@ -36,15 +32,16 @@ singleton_regex = {
           r'[+-][01][0-3](?:00|30|45)|[\+\-](?:1[0-3]|0[0-9])(?:00|30|45))',
     # timezone like 'UTC', 'UTC + 8:30', 'GMT+1130', '+0600'
     # reference: https://en.wikipedia.org/wiki/Time_zone
-    'splitters': r'(?:[,/ \.\-]? ?)'
+    # 'splitters': r'(?:[,/ \.\-]? ?)',
+    'splitters': r'(?: of | del | de | el |\. |, |-|/| ?)'
 }
 
 units = {
     'Y': ['%Y', '%y'],
-    'M': ['%B', '%b', '%m', '%-m'],
-    'D': ['%d', '%-d', ['%d', 'd_suffix'], ['%-d', 'd_suffix']],
+    'M': ['%B', '%b', '%m'],
+    'D': ['%d', ['%d', 'd_suffix']],
     'W': ['%A', '%a'],
-    'HOUR': ['%I', '%-I', '%H', '%-H'],
+    'HOUR': ['%I', '%H'],
     'MIN': '%M',
     'SEC': '%S',
     'MARK': '%p',
@@ -52,12 +49,49 @@ units = {
     'SINGLE_MONTH': ['%B', '%b']
 }
 
+spanish_to_english = {
+    'enero': 'January',
+    'febrero': 'February',
+    'marzo': 'March',
+    'abril': 'April',
+    'mayo': 'May',
+    'junio': 'June',
+    'julio': 'July',
+    'agosto': 'August',
+    'septiembre': 'September',
+    'octubre': 'October',
+    'noviembre': 'November',
+    'diciembre': 'December',
+    'feb': 'Feb',
+    'abr': 'Apr',
+    'set': 'Sept',
+    'dic': 'Dec',
+    'lunes': 'Monday',
+    'martes': 'Tuesday',
+    'miércoles': 'Wednesday',
+    'jueves': 'Thursday',
+    'viernes': 'Friday',
+    'sábado': 'Saturday',
+    'domingo': 'Sunday',
+    'L': 'Mon',
+    'M': 'Tue',
+    'X': 'Wed',
+    'J': 'Thu',
+    'V': 'Fri',
+    'S': 'Sat',
+    'D': 'Sun'
+}
+
+language_date_order = {
+    'es': 'DMY',
+    'en': 'MDY'
+}
+
 # relative date:
 # before, after, last, next, ago, later, in
 # number, half, quarter, several?, some?,
 # weeks, years, days, months, hours, minutes, seconds
 # now, , yesterday, tomorrow
-
 directions = {
     'last': '-',
     'next': '+',
@@ -67,7 +101,6 @@ directions = {
     'ago': '-',
     'later': '+'
 }
-
 num_to_digit = {
     'one': '1',
     'two': '2',
@@ -80,7 +113,6 @@ num_to_digit = {
     'nine': '9',
     'ten': '10'
 }
-
 spacy_rules = {
   "field_name": "relative_date",
   "rules": [
@@ -515,85 +547,3 @@ spacy_rules = {
     }
   ]
 }
-
-
-def generate_regex_for_a_unit(key_list):
-    if isinstance(key_list, list):
-        if not key_list or len(key_list) == 0:
-            return {'res': r'', 'pattern_list': []}
-        pattern_list = []
-        regex_list = []
-        for key in key_list:
-            if isinstance(key, list):
-                regex = singleton_regex[key[0]]
-                if key[0][0] == '%':
-                    pattern_list.append(key[0])
-                for sub_key in key[1:]:
-                    regex = regex + singleton_regex[sub_key]
-                    if sub_key[0] == '%':
-                        pattern_list.append(sub_key)
-                regex = regex
-                regex_list.append(regex)
-            else:
-                regex_list.append(singleton_regex[key])
-                if key[0] == '%':
-                    pattern_list.append(key)
-        if not regex_list or len(regex_list) == 0:
-            return {'res': r'', 'pattern_list': []}
-        res = r'(?:' + regex_list[0]
-        for r in regex_list[1:]:
-            res = res + '|' + r
-        res = res + ')'
-        return {'res': res, 'pattern_list': pattern_list}
-    else:
-        return {'res': r'(?:' + singleton_regex[key_list] + r')',
-                'pattern_list': [key_list] if key_list[0] == '%' else []}
-
-
-def generate_all_regexes_by_singletons():
-    all = {}
-    pattern = []
-    for k in units.keys():
-        unit = generate_regex_for_a_unit(units[k])
-        # print(unit)
-        all[k] = unit
-    # print(pattern)
-    s = singleton_regex['splitters']
-
-    time = r'(?:' + all['HOUR']['res'] + r': ?' + all['MIN']['res'] + r'(?:: ?' + all['SEC']['res'] + r')?(?:' + all[
-        'MARK']['res'] + ')?(?:' + s + all['TZ']['res'] + r')?)'
-    st = r'(?:' + singleton_regex['splitters'] + r'|T)?'
-    time_reg = r'(?:T?' + st + time + r'Z?)?'
-
-    week_reg_post = r'(?:' + s + all['W']['res'] + r')?'
-    week_reg_pre = r'(?:' + all['W']['res'] + s + r')?'
-
-    day_reg_post = r'(?:' + s + all['D']['res'] + r')?'
-    day_reg_pre = r'(?:' + all['D']['res'] + s + r')?'
-
-    pl = 'pattern_list'
-    time_pattern = all['HOUR'][pl] + all['MIN'][pl] + all['SEC'][pl] + all['MARK'][pl] + all['TZ'][pl]
-
-    # print(all['SINGLE'])
-
-    return ({
-                'MDY': r'(?<=\b)(?:(?:' + week_reg_pre + all['M']['res'] + day_reg_post + s + all['Y'][
-                    'res'] + week_reg_post + r')' + time_reg + r')(?=\b)',
-                'DMY': r'(?<=\b)(?:(?:' + week_reg_pre + day_reg_pre + all['M']['res'] + s + all['Y'][
-                    'res'] + week_reg_post + r')' + time_reg + r')(?=\b)',
-                'YMD': r'(?<=\b)(?:(?:' + week_reg_pre + all['Y']['res'] + s + all['M'][
-                    'res'] + day_reg_post + week_reg_post + r')' + time_reg + r')(?=\b)',
-                'SINGLE_MONTH': r'(?<=\b)(?:(?:' + all['SINGLE_MONTH']['res'] + r')' + time_reg + r')(?=\b)'
-            }, {
-                'MDY': all['W'][pl] + all['M'][pl] + all['D'][pl] + all['Y'][pl] + all['W'][pl] + time_pattern,
-                'DMY': all['W'][pl] + all['D'][pl] + all['M'][pl] + all['Y'][pl] + all['W'][pl] + time_pattern,
-                'YMD': all['W'][pl] + all['Y'][pl] + all['M'][pl] + all['D'][pl] + all['W'][pl] + time_pattern,
-                'SINGLE_MONTH': all['SINGLE_MONTH'][pl] + time_pattern
-            })
-
-#
-# temp = generate_all_regexes_by_singletons()
-# print(json.dumps(temp, indent=2))
-# final_regex = temp[0]
-# symbol_list = temp[1]
-
--- examples/date_extractor/date_extraction.py
+++ examples/date_extractor/date_extraction.py
@@ -1,4 +1,4 @@
-import json, os, sys, datetime
+import json, os, sys, datetime, time
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.append(os.path.join(os.path.dirname(__file__), '../..'))
 from etk.etk import ETK
@@ -16,17 +16,25 @@ sample_input = {
         },
         {
             "name": "Ryan",
-            "description": "1989-12-12 03/05/2018: I went to USC on Aug 20th, 2016 and will graduate on 2018, May 11. "
-                           "My birthday is 29-04-1994. I will graduate in 5 days. My classmates traveled to Hawaii last month. "
-                           "I hope that I could have a vocation after two weeks. Last year I was very busy. yesterday, The day after Tomorrow"
+            "description": "I will graduate in 5 days. My classmates traveled to Hawaii last month. "
+                           "I hope that I could have a vocation after two weeks. Last year I was very busy. "
+                           "yesterday, The day after Tomorrow"
         },
         {
             "name": "Selina",
             "description": "May June JULY march dec"
+        },
+        {
+            "name": "Sara",
+            "description": "el 29 de febrero de 1996 vs lunes, el 24 de junio, 2013 vs 3 de octubre de 2017"
+        },
+        {
+            "name": "Debbie",
+            "description": "2010@3@29  Jul.$15$17     Thur 2018, Apr. 5"
         }
     ]
 }
-
+start = time.time()
 
 etk = ETK()
 doc = etk.create_document(sample_input)
@@ -36,7 +44,7 @@ member_descriptions = doc.select_segments("members[*].description")
 members = doc.select_segments("members[*]")
 
 ignore_before = datetime.datetime(1890, 1, 1)
-ignore_after = datetime.datetime(2100, 10, 10)
+ignore_after = datetime.datetime(2500, 10, 10)
 relative_base = datetime.datetime(2018, 1, 1)
 
 for m_d, m in zip(member_descriptions, members):
@@ -45,9 +53,7 @@ for m_d, m in zip(member_descriptions, members):
         m_d,
         extract_first_date_only=False,   # first valid
 
-        additional_formats=['%d-%m-%Y', '%b %dth, %Y'],
-
-        # match relax or strict / can be partial match ?     # TODO
+        additional_formats=['%Y@%m@%d', '%a %Y, %b %d'],
 
         use_default_formats=True,
 
@@ -57,14 +63,14 @@ for m_d, m in zip(member_descriptions, members):
         # ignore_dates_after: datetime.datetime = None,
         ignore_dates_after=ignore_after,
 
-        detect_relative_dates=True,
+        detect_relative_dates=False,
 
         relative_base=relative_base,
 
         # preferred_date_order: str = "MDY",  # used for interpreting ambiguous dates that are missing parts
         preferred_date_order="DMY",
 
-        prefer_language_date_order=True,    # TODO
+        prefer_language_date_order=True,
 
         # timezone: str = None,  # default is local timezone.
         # timezone='GMT',
@@ -72,8 +78,8 @@ for m_d, m in zip(member_descriptions, members):
         # to_timezone: str = None,  # when not specified, not timezone conversion is done.
         # to_timezone='UTC',
 
-        # return_as_timezone_aware: bool = True,  # ?
-        return_as_timezone_aware=True,     # TODO
+        # return_as_timezone_aware: bool = True
+        return_as_timezone_aware=False,
 
         # prefer_day_of_month: str = "first",  # can be "current", "first", "last".
         prefer_day_of_month='first',
@@ -82,7 +88,7 @@ for m_d, m in zip(member_descriptions, members):
         prefer_dates_from='future',
     )
     m.store_extractions(dates, "related_dates")
-
-# TODO: validate the date by day of the week if available
+    print(len(dates))
 
 print(json.dumps(sample_input, indent=2))
+print(time.time()-start)
