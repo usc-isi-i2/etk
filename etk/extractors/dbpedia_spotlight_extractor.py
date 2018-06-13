bad2775ad2112c40fb63a98d786d51e23b51854c
xkgoodbest <xkgoodbest@163.com>
2018-06-13 13:50:29 -0700

    dbpedia
    Ke Xu

--- etk/extractors/dbpedia_spotlight_extractor.py
+++ etk/extractors/dbpedia_spotlight_extractor.py
@@ -1,18 +1,23 @@
-from etk.extractor import Extractor
+from etk.extractor import Extractor, InputType
 from etk.extraction import Extraction
 from typing import List
-
+import requests
 
 
 class DBpediaSpotlightExtractor(Extractor):
-    def __init__(self):
+    search_url = ''
+
+    def __init__(self, extractor_name: str, url: str):
+        Extractor.__init__(self, input_type=InputType.TEXT,
+                           category="build_in_extractor",
+                           name=extractor_name)
+        self.search_url = url
         # TODO initialise stuff
         # TODO if using the dbpedia webservice, a class variable should be set with the url
         # TODO if using the codebase, the library can be setup to called here
         # TODO if using the codebase, we need a local dbpedia against which the queries are to be made, set it up here as well
-        pass
 
-    def extract(self, text: str) -> List[Extraction]:
+    def extract(self, text: str, confidence: float, filter: list) -> List[Extraction]:
         """
             Extract with the input text
             Args:
@@ -20,6 +25,42 @@ class DBpediaSpotlightExtractor(Extractor):
 
             Returns: List[Extraction]
         """
-        # TODO add code here to call the dbpedia spotlight and to extract the entities, create a Extraction object and return a list of Extraction objects
-        return
 
+        self.filter = ','.join(filter)
+
+        # url = 'http://api.dbpedia-spotlight.org/en/annotate'
+
+        # url = 'http://model.dbpedia-spotlight.org/en/annotate'
+        # filters='PREFIX dbo: <http://dbpedia.org/ontology/> '+'SELECT DISTINCT ?'+filter+' WHERE  { ?'+filter+' rdf:type dbo:'+filter +'}'
+        # search_data = [('confidence', self.search_confidence), ('text', text),('sparql',filters)]
+
+        search_data = [('confidence', confidence),
+                       ('text', text),
+                       ('types', self.filter)]
+        search_headers = {'Accept': 'application/json'}
+
+        r = requests.post(self.search_url,
+                          data=search_data,
+                          headers=search_headers)
+        results = r.json()
+        last_results = self.combiner(results)
+        # TODO add code here to call the dbpedia spotlight and to extract the entities, create a Extraction object and return a list of Extraction objects
+        return last_results
+
+    # combiner function is used to form results in to Extraction object
+    def combiner(self, results: dict) -> List[Extraction]:
+        return_result = []
+        if "Resources" in results:
+            resources_results = results["Resources"]
+            for one_result in resources_results:
+                types = one_result['@types'].split(',')
+                return_result.append(Extraction(confidence=float(results['@confidence']),
+                                                extractor_name=self.name,
+                                                start_char=int(one_result['@offset']),
+                                                end_char=int(one_result['@offset']) + len(one_result['@surfaceForm']),
+                                                value={'surfaceForm': one_result['@surfaceForm'],
+                                                       'URI': one_result['@URI'],
+                                                       'types': types,
+                                                       'similarityScore': float(one_result['@similarityScore'])}))
+            return return_result
+        return []
---
+++ etk/unit_tests/test_DBPediaSpotlight_extractor.py
@@ -0,0 +1,114 @@
+import unittest
+import json
+from etk.extractors.dbpedia_spotlight_extractor import DBpediaSpotlightExtractor
+
+
+class TestDBPediaSpotlightExtractor(unittest.TestCase):
+
+    def test_DBPediaSpotlight_extractor(self) -> None:
+        text = 'Cohen has not been charged with a crime, but the FBI raid of his home, \
+        hotel room and office in early April revealed that prosecutors had zeroed in on \
+        his personal financial dealings, including the payment he made to porn star \
+        Stormy Daniels on Trump\'s behalf before the election.'
+        filters = ['Person', 'Place', 'Organisation']
+        confidence = 0.5
+
+        extractor = DBpediaSpotlightExtractor(extractor_name='dbPedia_extractor',
+                                              url='http://model.dbpedia-spotlight.org/en/annotate')
+        results = extractor.extract(text, confidence, filters)
+
+        if results == []:
+            self.assertEqual(results, [])
+
+        else:
+            extracted = []
+            for i in results:
+                extracted_value = {
+                    'value': i.value,
+                    'confidence': i.confidence,
+                    'start_char': i.provenance['start_char'],
+                    'end_char': i.provenance['end_char'],
+                }
+                extracted.append(extracted_value)
+
+            extracted_json = json.dumps(extracted)
+            # extracted_json = json.loads(extracted_json)
+            # print(extracted_json)
+            expected = [{"value": {"surfaceForm": "FBI",
+                                   "URI": "http://dbpedia.org/resource/Federal_Bureau_of_Investigation",
+                                   "types": ["Wikidata:Q43229",
+                                             "Wikidata:Q327333",
+                                             "Wikidata:Q24229398",
+                                             "DUL:SocialPerson",
+                                             "DUL:Agent",
+                                             "Schema:Organization",
+                                             "DBpedia:Organisation",
+                                             "DBpedia:GovernmentAgency",
+                                             "DBpedia:Agent"],
+                                   "similarityScore": 0.9999999999998863},
+                         "confidence": 0.5,
+                         "start_char": 49,
+                         "end_char": 52},
+
+                        {"value": {"surfaceForm": "financial dealings",
+                                   "URI": "http://dbpedia.org/resource/Yasser_Arafat",
+                                   "types": ["Http://xmlns.com/foaf/0.1/Person",
+                                             "Wikidata:Q82955",
+                                             "Wikidata:Q5",
+                                             "Wikidata:Q30461",
+                                             "Wikidata:Q24229398",
+                                             "Wikidata:Q215627",
+                                             "DUL:NaturalPerson",
+                                             "DUL:Agent",
+                                             "Schema:Person",
+                                             "DBpedia:President",
+                                             "DBpedia:Politician",
+                                             "DBpedia:Person",
+                                             "DBpedia:Agent"],
+                                   "similarityScore": 0.9999999999648139},
+                         "confidence": 0.5,
+                         "start_char": 180,
+                         "end_char": 198},
+
+                        {"value": {"surfaceForm": "Stormy Daniels",
+                                   "URI": "http://dbpedia.org/resource/Stormy_Daniels",
+                                   "types": ["Http://xmlns.com/foaf/0.1/Person",
+                                             "Wikidata:Q5",
+                                             "Wikidata:Q488111",
+                                             "Wikidata:Q483501",
+                                             "Wikidata:Q33999",
+                                             "Wikidata:Q24229398",
+                                             "Wikidata:Q215627",
+                                             "DUL:NaturalPerson",
+                                             "DUL:Agent",
+                                             "Schema:Person",
+                                             "DBpedia:Person",
+                                             "DBpedia:Artist",
+                                             "DBpedia:Agent",
+                                             "DBpedia:AdultActor",
+                                             "DBpedia:Actor"],
+                                   "similarityScore": 1.0},
+                         "confidence": 0.5,
+                         "start_char": 251,
+                         "end_char": 265},
+
+                        {"value": {"surfaceForm": "Trump",
+                                   "URI": "http://dbpedia.org/resource/Donald_Trump",
+                                   "types": ["Http://xmlns.com/foaf/0.1/Person",
+                                             "Wikidata:Q5",
+                                             "Wikidata:Q24229398",
+                                             "Wikidata:Q215627",
+                                             "DUL:NaturalPerson",
+                                             "DUL:Agent",
+                                             "Schema:Person",
+                                             "DBpedia:Person",
+                                             "DBpedia:Agent"],
+                                   "similarityScore": 0.9888612234881796},
+                         "confidence": 0.5,
+                         "start_char": 269,
+                         "end_char": 274}]
+            self.assertEqual(extracted, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
