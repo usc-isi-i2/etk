5fbe867b3f1c5c2f7ebb9abf7e7424e9e4cdcf15
Xi Jin <fate_stigma@hotmail.com>
2018-06-23 11:38:52 -0700

    Ontology namespacemanager if already used, raise an error

--- etk/ontology_namespacemanager.py
+++ etk/ontology_namespacemanager.py
@@ -2,10 +2,11 @@ import re
 from rdflib.namespace import OWL, Namespace, NamespaceManager
 from rdflib import URIRef
 
+
 SCHEMA = Namespace('http://schema.org/')
 DIG = Namespace('http://dig.isi.edu/ontologies/dig/')
 
-URI_PATTERN = re.compile(r'^http\:|^urn\:|^info\:|^ftp\:|^https\:')
+URI_PATTERN = re.compile(r'^http:|^urn:|^info:|^ftp:|^https:')
 URI_ABBR_PATTERN = re.compile(r'^([^:]+):([^:]+)$')
 
 
@@ -17,6 +18,10 @@ class PrefixNotFoundException(Exception):
     pass
 
 
+class PrefixAlreadyUsedException(Exception):
+    pass
+
+
 class OntologyNamespaceManager(NamespaceManager):
     def __init__(self, *args, **kwargs):
         super(OntologyNamespaceManager, self).__init__(*args, **kwargs)
@@ -24,7 +29,7 @@ class OntologyNamespaceManager(NamespaceManager):
         self.bind('schema', SCHEMA)
         self.bind('dig', DIG)
 
-    def parse_uri(self, text):
+    def parse_uri(self, text: str) -> URIRef:
         """
         Parse input text into URI
         text can be:
@@ -48,4 +53,38 @@ class OntologyNamespaceManager(NamespaceManager):
                     return URIRef(base + name)
         raise WrongFormatURIException()
 
+    def bind(self, prefix: str, namespace: str, override=True, replace=False):
+        """
+        bind a given namespace to the prefix
 
+        if override, rebind, even if the given namespace is already
+        bound to another prefix.
+
+        if replace, replace any existing prefix with the new namespace
+
+        """
+
+        namespace = URIRef(str(namespace))
+        # When documenting explain that override only applies in what cases
+        if prefix is None:
+            prefix = ''
+        bound_namespace = self.store.namespace(prefix)
+        # Check if the bound_namespace contains a URI and if so convert it into a URIRef for
+        # comparison. This is to prevent duplicate namespaces with the same URI.
+        if bound_namespace:
+            bound_namespace = URIRef(bound_namespace)
+        if bound_namespace and bound_namespace != namespace:
+
+            if replace:
+                self.store.bind(prefix, namespace)
+            # prefix already in use for different namespace
+            raise PrefixAlreadyUsedException
+        else:
+            bound_prefix = self.store.prefix(namespace)
+            if bound_prefix is None:
+                self.store.bind(prefix, namespace)
+            elif bound_prefix == prefix:
+                pass  # already bound
+            else:
+                if override or bound_prefix.startswith("_"):
+                    self.store.bind(prefix, namespace)
