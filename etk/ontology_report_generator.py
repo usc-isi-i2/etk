4311d200879aaa26ee1c933c683aaec7104fe7d8
Xi Jin <fate_stigma@hotmail.com>
2018-07-03 23:15:06 -0700

    Moved tool in bin folder

--- etk/ontology_report_generator.py
+++ etk/ontology_report_generator.py
@@ -224,33 +224,3 @@ class OntologyReportGenerator:
         kind = {OntologyClass: 'C', OntologyObjectProperty: 'O', OntologyDatatypeProperty: 'D'}
         return template.format(kind[type(e)], e.uri(), e.name())
 
-
-if __name__ == '__main__':
-    import argparse
-
-    parser = argparse.ArgumentParser(description='Generate HTML report for the input ontology files')
-    parser.add_argument('files', nargs='+', help='Input turtle files.')
-    parser.add_argument('--no-validation', action='store_false', dest='validation', default=True,
-                        help='Don\'t perform domain and range validation.')
-    parser.add_argument('-o', '--output', dest='out', default='ontology-doc.html',
-                        help='Location of generated HTML report.')
-    parser.add_argument('-i', '--include-undefined-classes', action='store_true',
-                        dest='include_class', default=False, help='Include those undefined classes '
-                                                                  'but referenced by others.')
-    parser.add_argument('-t', '--include-turtle', action='store_true', dest='include_turtle',
-                        default=False, help='Include turtle related to this entity. NOTE: this may '
-                                            'takes longer time.')
-    parser.add_argument('-q', '--quiet', action='store_true', dest='quiet', default=False,
-                        help='Suppress warning.')
-    parser.add_argument('--exclude-warning', action='store_true', dest='exclude_warning',
-                        default=False, help='Exclude warning messages in HTML report')
-    args = parser.parse_args()
-
-    contents = [open(f).read() for f in args.files]
-    ontology = Ontology(contents, validation=args.validation, include_undefined_class=args.include_class,
-                        quiet=args.quiet)
-    doc_content = OntologyReportGenerator(ontology).generate_html_report(include_turtle=args.include_turtle,
-                                                                         exclude_warning=args.exclude_warning)
-
-    with open(args.out, "w") as f:
-        f.write(doc_content)
