376281c48af696ac19519d31abe5cf34f2d46e68
Amandeep Singh <amandeep.s.saggu@gmail.com>
2018-08-09 15:59:12 -0700

    ability to pass nlp to spacy ner and also remove loading of spacy model every time extract is called

--- etk/extractors/spacy_ner_extractor.py
+++ etk/extractors/spacy_ner_extractor.py
@@ -5,16 +5,16 @@ from typing import List
 
 
 class SpacyNerExtractor(Extractor):
-    def __init__(self, extractor_name: str):
+    def __init__(self, extractor_name: str, nlp=spacy.load('en_core_web_sm')):
         Extractor.__init__(self, input_type=InputType.TEXT,
                            category="built_in_extractor",
                            name=extractor_name)
+        self.nlp = nlp
 
     # all_attrs = ['PERSON', 'NORP', 'FAC', 'ORG', 'GPE', 'LOC', 'PRODUCT', 'EVENT', 'WORK_OF_ART', 'LAW', 'LANGUAGE',
     #              'DATE', 'TIME', 'PERCENT', 'MONEY', 'QUANTITY', 'ORDINAL', 'CARDINAL']
     def extract(self, text: str, get_attr=['PERSON', 'ORG', 'GPE']) -> List[Extraction]:
-        nlp = spacy.load('en_core_web_sm')
-        doc = nlp(text)
+        doc = self.nlp(text)
         attr_list = list()
         for ent in doc.ents:
             if ent.label_ in get_attr:
