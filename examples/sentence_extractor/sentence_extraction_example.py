0c5625a9d6e8563fba2856e7f0c2efce060f31c6
Amandeep Singh <amandeep.s.saggu@gmail.com>
2018-08-13 19:00:12 -0700

    change sentence splitter to work with files

--- examples/sentence_extractor/sentence_extraction_example.py
+++ examples/sentence_extractor/sentence_extraction_example.py
@@ -7,6 +7,7 @@ from etk.etk import ETK
 from etk.document import Document
 from etk.etk_module import ETKModule
 from etk.extractors.sentence_extractor import SentenceExtractor
+from optparse import OptionParser
 
 
 class SentenceSplittingETKModule(ETKModule):
@@ -23,69 +24,88 @@ class SentenceSplittingETKModule(ETKModule):
         Add your code for processing the document
         """
 
-        text_to_be_split = doc.select_segments("doc_text[*].text")
-        units_of_text = doc.select_segments("doc_text[*]")
-
-        for t, u in zip(text_to_be_split, units_of_text):
-            split_sentences = doc.extract(self.sentence_extractor, t)
-            u.store(split_sentences, "split_sentences")
-
+        text_to_be_split = doc.select_segments("lexisnexis.doc_description")
+        split_sentences = doc.extract(self.sentence_extractor, text_to_be_split)
+        doc.store(split_sentences, 'split_sentences')
+        # for t, u in zip(text_to_be_split, units_of_text):
+        #     split_sentences = doc.extract(self.sentence_extractor, t)
+        #     u.store(split_sentences, "split_sentences")
         return list()
 
 
 if __name__ == "__main__":
 
-    toy_doc = {
-        "doc_text": [
-            {
-                "name": "Control - Single Sentence",
-                "description": "This sentence should not be split",
-                "text": "How much wood could a woodchuck chuck if a woodchuck "
-                        "had long arms?"
-            },
-            {
-                "name": "Control - Conversational Text",
-                "description": "These sentences should split into three parts",
-                "text": "The fact of the matter is this: nobody has ever seen "
-                        "a woodchuck go super-sayan. Even if it were possible, "
-                        "the likelihood that they would actually throw a piece "
-                        "of wood is preposterously low. It's just not going to happen!"
-            },
-            {
-                "name": "Test1 - Butchered Conversational Text",
-                "description": "Modified sentence boundary conditions",
-                "text": "The fact of the matter is this: Nobody has ever seen "
-                        "a woodchuck go super-sayan.even if it were possible, "
-                        "the likelihood that they would actually throw a piece "
-                        "of wood is preposterously low. it's just not going to happen!"
-            },
-            {
-                "name": "Test2 - Butchered Wikipedia Article",
-                "description": "Similarly modified sentence boundary conditions, "
-                               "but this text is more regular in structure",
-                "text": "The groundhog (Marmota monax), also known as a woodchuck, "
-                        "is a rodent of the family Sciuridae, belonging to the group "
-                        "of large ground squirrels known as marmots.It was first "
-                        "scientifically described by Carl Linnaeus in 1758...the "
-                        "groundhog is also referred to as a chuck, wood-shock, "
-                        "groundpig, whistlepig, whistler, thickwood badger, "
-                        "Canada marmot, monax, moonack, weenusk, red monk and, "
-                        "among French Canadians in eastern Canada, siffleur"
-            },
-            {
-                "name": "Test3 - Social Media",
-                "description": "Parser stress test for tweets",
-                "text": "Slides onto twitter..... \n"
-                        ".......slippery floor....... \n"
-                        "............slides out the other side..."
-            }
-        ],
-        "doc_id": 42069
-    }
-
+    # toy_doc = {
+    #     "doc_text": [
+    #         {
+    #             "name": "Control - Single Sentence",
+    #             "description": "This sentence should not be split",
+    #             "text": "How much wood could a woodchuck chuck if a woodchuck "
+    #                     "had long arms?"
+    #         },
+    #         {
+    #             "name": "Control - Conversational Text",
+    #             "description": "These sentences should split into three parts",
+    #             "text": "The fact of the matter is this: nobody has ever seen "
+    #                     "a woodchuck go super-sayan. Even if it were possible, "
+    #                     "the likelihood that they would actually throw a piece "
+    #                     "of wood is preposterously low. It's just not going to happen!"
+    #         },
+    #         {
+    #             "name": "Test1 - Butchered Conversational Text",
+    #             "description": "Modified sentence boundary conditions",
+    #             "text": "The fact of the matter is this: Nobody has ever seen "
+    #                     "a woodchuck go super-sayan.even if it were possible, "
+    #                     "the likelihood that they would actually throw a piece "
+    #                     "of wood is preposterously low. it's just not going to happen!"
+    #         },
+    #         {
+    #             "name": "Test2 - Butchered Wikipedia Article",
+    #             "description": "Similarly modified sentence boundary conditions, "
+    #                            "but this text is more regular in structure",
+    #             "text": "The groundhog (Marmota monax), also known as a woodchuck, "
+    #                     "is a rodent of the family Sciuridae, belonging to the group "
+    #                     "of large ground squirrels known as marmots.It was first "
+    #                     "scientifically described by Carl Linnaeus in 1758...the "
+    #                     "groundhog is also referred to as a chuck, wood-shock, "
+    #                     "groundpig, whistlepig, whistler, thickwood badger, "
+    #                     "Canada marmot, monax, moonack, weenusk, red monk and, "
+    #                     "among French Canadians in eastern Canada, siffleur"
+    #         },
+    #         {
+    #             "name": "Test3 - Social Media",
+    #             "description": "Parser stress test for tweets",
+    #             "text": "Slides onto twitter..... \n"
+    #                     ".......slippery floor....... \n"
+    #                     "............slides out the other side..."
+    #         }
+    #     ],
+    #     "doc_id": 42069
+    # }
+    #
     etk = ETK(modules=SentenceSplittingETKModule)
-    doc = etk.create_document(toy_doc)
+    # doc = etk.create_document(toy_doc)
+    #
+    # split_doc = etk.process_ems(doc)
+    #
+    # print(json.dumps(split_doc[0].value, indent=2))
+    parser = OptionParser(conflict_handler="resolve")
+    parser.add_option("-i", "--input_file", action="store",
+                      type="string", dest="input_file")
+    parser.add_option("-o", "--output_file", action="store_true",
+                      type="string", dest="output_file")
+    (c_options, args) = parser.parse_args()
 
-    split_doc = etk.process_ems(doc)
+    input_file = c_options.input_file
+    output_file = c_options.output_file
 
-    print(json.dumps(split_doc[0].value, indent=2))
+    f = open(input_file, 'r', 'utf-8')
+    o = open(output_file, 'w', 'utf-8')
+    for line in f:
+        json_x = json.loads(line)
+        doc = etk.create_document(json_x)
+        doc.doc_id = json_x['doc_id']
+        sentences = etk.process_ems(doc)
+        for s in sentences:
+            o.write(json.dumps(s.value))
+            o.write('\n')
