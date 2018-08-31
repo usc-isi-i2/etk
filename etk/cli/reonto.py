4085d415012994b86fabfd8f0dbcecdb666b1a8f
greatyyx <bigyyx@gmail.com>
2018-08-31 10:48:11 -0700

    add support for sub graphs

--- etk/cli/reonto.py
+++ etk/cli/reonto.py
@@ -1,24 +1,22 @@
-import rdflib
+from rdflib.graph import Graph, ConjunctiveGraph, Dataset, URIRef
 from rdflib.plugins.sparql import prepareQuery
 import argparse
 import warnings
+import os
 
 
-def read_input_file(f, chunk_size):
+def read_by_chunk(f, chunk_size):
     i = 0
     buff = ''
 
     for line in f:
-
-        if i >= chunk_size:
-            yield buff
-
-            i = 0
-            buff = ''
-
+        if chunk_size != 0:
+            if i >= chunk_size:
+                yield buff
+                i = 0
+                buff = ''
         buff += line + '\n'
         i += 1
-
     yield buff
 
 
@@ -34,7 +32,8 @@ def add_arguments(parser):
     parser.add_argument('-o', '--output-file', type=argparse.FileType('w'), dest='output_file')
     parser.add_argument('--output-type', action='store', dest='output_type', default='nt')
     parser.add_argument('-q', '--query-file', type=argparse.FileType('r'), dest='query_file')
-    parser.add_argument('-s', '--chunk-size', action='store', dest='chunk_size', default=1000)
+    parser.add_argument('-s', '--chunk-size', action='store', dest='chunk_size', default=0)
+    parser.add_argument('-g', '--graph', type=argparse.FileType('r'), dest='graphs', nargs='+', default=[])
 
 
 def run(args):
@@ -47,13 +46,20 @@ def run(args):
         warnings.simplefilter('ignore')
 
         query = prepareQuery(args.query_file.read())
-        # query = args.query_file.read()
 
-        for data in read_input_file(args.input_file, args.chunk_size):
-            g = rdflib.Graph()
+        ds = Dataset()
+        for f in args.graphs:
+            g = Graph(identifier=os.path.basename(f.name))
+            g.parse(data=f.read(), format='n3')
+            ds.add_graph(g)
+
+        for data in read_by_chunk(args.input_file, int(args.chunk_size)):
+            g = Graph(identifier='data')
             g.parse(data=data, format=args.input_type)
-            res = g.query(query)
+            ds.add_graph(g)
+            res = ds.query(query)
             res = res.serialize(format=args.output_type)
             if res == b'\n':
                 continue
             args.output_file.write(res.decode('utf-8'))
+            ds.remove_graph(g)
