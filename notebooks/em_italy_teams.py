772a98d8daed07bbfc9eae38c425668749e44e42
xkgoodbest <xkgoodbest@163.com>
2018-09-13 16:20:33 -0700

    bug fix on tutorial, py file created

--- notebooks/em_italy_teams.py
+++ notebooks/em_italy_teams.py
@@ -1,5 +1,6 @@
 import os
-import sys, json
+import sys
+import json
 import requests
 import jsonpath_ng.ext as jex
 import re
@@ -11,27 +12,29 @@ from etk.utilities import Utility
 from etk.extractors.table_extractor import TableExtractor
 from etk.extractors.glossary_extractor import GlossaryExtractor
 
+
 class ItalyTeamsModule(ETKModule):
     def __init__(self, etk):
         ETKModule.__init__(self, etk)
-        my_table_extractor = TableExtractor()
+        self.my_table_extractor = TableExtractor()
 
         file_name = './resources/cities_ppl>25000.json'
         file = open(file_name, 'r')
-        city_dataset = json.loads(file.read())
+        self.city_dataset = json.loads(file.read())
         file.close()
-        city_list = list(city_dataset.keys())
+        self.city_list = list(self.city_dataset.keys())
 
-        my_glossary_extractor = GlossaryExtractor(glossary=city_list, extractor_name='tutorial_glossary',
-                                                  tokenizer=etk.default_tokenizer, ngrams=3,
-                                                  case_sensitive=False)
+        self.my_glossary_extractor = GlossaryExtractor(glossary=self.city_list, extractor_name='tutorial_glossary',
+                                                       tokenizer=etk.default_tokenizer, ngrams=3,
+                                                       case_sensitive=False)
 
-    def process_document(self, cdr_doc):
+    def process_document(self, cdr_doc: Document):
         new_docs = list()
         doc_json = cdr_doc.cdr_document
 
         if 'raw_content' in doc_json and doc_json['raw_content'].strip() != '':
-            tables_in_page = my_table_extractor.extract(doc_json['raw_content'])[:14]
+            tables_in_page = self.my_table_extractor.extract(
+                doc_json['raw_content'])[:14]
             for table in tables_in_page:
 
                 # skipping the first row, the heading
@@ -39,7 +42,6 @@ class ItalyTeamsModule(ETKModule):
                     doc = etk.create_document(row)
                     all_json_path = '$.cells[0:4].text'
                     row_values = doc.select_segments(all_json_path)
-
                     # add the information we extracted in the knowledge graph of the doc.
                     doc.kg.add_value('team', value=row_values[0].value)
                     doc.kg.add_value('city_name', value=row_values[1].value)
@@ -54,15 +56,18 @@ class ItalyTeamsModule(ETKModule):
                     row_values = doc.select_segments(city_json_path)
 
                     # use the city field of the doc, run the GlossaryExtractor
-                    extractions = doc.extract(my_glossary_extractor, row_values[0])
+                    extractions = doc.extract(
+                        self.my_glossary_extractor, row_values[0])
                     if extractions:
-                        path = '$."' + extractions[0].value + '"[?(@.country == "Italy")]'
+                        path = '$."' + \
+                            extractions[0].value + '"[?(@.country == "Italy")]'
                         jsonpath_expr = jex.parse(path)
-                        city_match = jsonpath_expr.find(city_dataset)
+                        city_match = jsonpath_expr.find(self.city_dataset)
                         if city_match:
                             # add corresponding values of city_dataset into knowledge graph of the doc
                             for field in city_match[0].value:
-                                doc.kg.add_value(field, value=city_match[0].value[field])
+                                doc.kg.add_value(
+                                    field, value=city_match[0].value[field])
                     new_docs.append(doc)
         return new_docs
 
@@ -83,5 +88,6 @@ if __name__ == "__main__":
     etk = ETK(modules=ItalyTeamsModule, kg_schema=kg_schema)
     etk.parser = jex.parse
     cdr_doc = Document(etk, cdr_document=cdr, mime_type='json', url=url)
-    results = etk.process_ems(cdr_doc)
-
+    results = etk.process_ems(cdr_doc)[1:]
+    print("Sample result:\n")
+    print(json.dumps(results[0].value, indent=2))
