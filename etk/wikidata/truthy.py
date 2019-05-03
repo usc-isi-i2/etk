c670eacf9a9da3c92f185821987b6e0647907fdd
Xi Jin <fate_stigma@hotmail.com>
2019-05-02 17:31:25 -0700

    Speed up truthy updater

--- etk/wikidata/truthy.py
+++ etk/wikidata/truthy.py
@@ -1,93 +1,76 @@
 from rdflib.plugins.stores.sparqlstore import SPARQLUpdateStore
+from rdflib.query import ResultException
 
 
-class NodeProperty:
-    def __init__(self, node, property_, endpoint: SPARQLUpdateStore, dryrun=False):
-        self.node = node
-        self.prop = property_
-        self.endpoint = endpoint
+class TruthyUpdater:
+    def __init__(self, endpoint, dryrun=False, user=None, passwd=None):
+        self.endpoint = SPARQLUpdateStore(endpoint,
+                                          default_query_method='POST')
+        if user:
+            self.endpoint.setCredentials(user, passwd)
         self.dryrun = dryrun
 
-    def ask(self, query):
-        for ans in self.endpoint.query(query):
-            return ans
+    def build_truthy(self, np_list):
+        self.insert_truthy_rank(np_list)
+        self.delete_normal_rank(np_list)
 
     def update(self, query):
         if self.dryrun:
             action = 'INSERT' if 'INSERT' in query else 'DELETE'
-            # address empty result with CONSTRUCT
-            # https://github.com/RDFLib/rdflib/issues/775
-            if self.ask('ASK ' + query[query.find('WHERE')+5:]):
-                res = self.endpoint.query(query.replace(action, 'CONSTRUCT'))
+            query = query.replace(action, 'CONSTRUCT')
+            # print(query)
+            try:
+                res = self.endpoint.query(query)
                 print('### About to {} following triples:'.format(action))
                 for row in res:
                     print(' '.join(e.n3() for e in row))
+            except ResultException:
+                print('Maybe empty')
         else:
             self.endpoint.update(query)
 
-    def has_truthy_preferred_rank(self):
+    def insert_truthy_rank(self, np_list):
+        values = ' '.join('( wd:%(node)s p:%(p)s ps:%(p)s psn:%(p)s wdt:%(p)s wdtn:%(p)s )' % {'node': n, 'p': p} for n, p in np_list)
         query = '''
-        ask { wd:%(node)s p:%(prop)s [ a wikibase:BestRank ; wikibase:rank wikibase:PreferredRank ] }
-        '''
-        query = query % {'node': self.node, 'prop': self.prop}
-        return self.ask(query)
-
-    def has_preferred_rank(self):
-        query = 'ask { wd:%(node)s p:%(prop)s/wikibase:rank wikibase:PreferredRank }'
-        query = query % {'node': self.node, 'prop': self.prop}
-        return self.ask(query)
-
-    def insert_other_rank_statements(self, rank):
-        query = '''
-          INSERT {
-            ?statement a wikibase:BestRank .
-            wd:%(node)s wdt:%(prop)s ?value .
-            wd:%(node)s wdtn:%(prop)s ?normalValue
-          } WHERE {
-            ?statement ^p:%(prop)s wd:%(node)s ;
-                        wikibase:rank wikibase:%(rank)s ;
-                        ps:%(prop)s ?value .
-            OPTIONAL { ?statement psn:%(prop)s ?normalValue }
-            FILTER NOT EXISTS { ?statement a wikibase:BestRank }
-          }
-        ''' % {'node': self.node, 'prop': self.prop, 'rank': rank}
+        INSERT {
+          ?statement a wikibase:BestRank .
+          ?node ?wdt ?psv ;
+                ?wdtn ?psnv .
+        } WHERE {
+          %s
+            {
+                ?node ?p ?statement .
+                ?statement wikibase:rank wikibase:PreferredRank ;
+                           ?ps ?psv .
+                OPTIONAL { ?statement ?psn ?psnv }
+                FILTER NOT EXISTS { ?statement a wikibase:BestRank }
+            }
+            UNION
+            {
+              ?node ?p ?statement .
+              ?statement wikibase:rank wikibase:NormalRank ;
+                         ?ps ?psv .
+              OPTIONAL { ?statement ?psn ?psnv }
+              FILTER NOT EXISTS { ?statement a wikibase:BestRank }
+              FILTER NOT EXISTS { ?node ?p [ wikibase:rank wikibase:PreferredRank ] }
+            }
+        }
+        ''' % ('VALUES (?node ?p ?ps ?psn ?wdt ?wdtn ) { %s }' % values)
         self.update(query)
 
-    def delete_truthy_normal_rank(self):
+    def delete_normal_rank(self, np_list):
+        values = ' '.join('( wd:%(node)s p:%(p)s wdt:%(p)s wdtn:%(p)s )' % {'node': n, 'p': p} for n, p in np_list)
         query = '''
           DELETE {
             ?statement a wikibase:BestRank .
-            wd:%(node)s wdt:%(prop)s ?value .
-            wd:%(node)s wdtn:%(prop)s ?normalValue
+            ?node ?wdt ?value ;
+              ?wdtn ?no .
           } WHERE {
+            %s
+            ?node ?p ?statement ;
+                  ?p [ wikibase:rank wikibase:PreferredRank ] .
             ?statement a wikibase:BestRank ;
-                       ^p:%(prop)s wd:%(node)s ;
-                       wikibase:rank wikibase:NormalRank ;
-                       ps:%(prop)s ?value .
-            OPTIONAL { ?statement psn:%(prop)s ?normalValue }
+                       wikibase:rank wikibase:NormalRank .
           }
-        ''' % {'node': self.node, 'prop': self.prop}
+        ''' % ('VALUES (?node ?p ?wdt ?wdtn ) { %s }' % values)
         self.update(query)
-
-
-class TruthyUpdater:
-    def __init__(self, endpoint):
-        self.endpoint = SPARQLUpdateStore(endpoint)
-
-    def update(self, node, property_, dryrun=False):
-        np = NodeProperty(node, property_, self.endpoint, dryrun)
-        if np.has_truthy_preferred_rank():
-            np.insert_other_rank_statements('PreferredRank')
-        elif np.has_preferred_rank():
-            np.delete_truthy_normal_rank()
-            np.insert_other_rank_statements('PreferredRank')
-        else:
-            np.insert_other_rank_statements('NormalRank')
-
-
-if __name__ == '__main__':
-    np = NodeProperty('Q42', 'P69', SPARQLUpdateStore('https://query.wikidata.org/sparql'), True)
-    print(np.has_truthy_preferred_rank())
-    print(np.has_preferred_rank())
-    np.delete_truthy_normal_rank()
-    np.insert_other_rank_statements('NormalRank')
