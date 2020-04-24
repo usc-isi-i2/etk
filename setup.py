cc64661574a3334b48c7bb8d3f6208a06600e75e
saggu <amandeep.s.saggu@gmail.com>
2020-04-24 15:17:18 -0700

    remoce landmark from requirements

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open('requirements.txt', 'r') as f:
 
 setuptools.setup(
     name="etk",
-    version="2.2.0",
+    version="2.2.1",
     author="Amandeep Singh",
     author_email="amandeep.s.saggu@gmail.com",
     description="extraction toolkit",
